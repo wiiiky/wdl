813ddbfad3406ddce0e5d6d430d794b541897a63
wiiiky <wiiiky@yeah.net>
2014-03-27 16:07:38 +0800

    添加下载小箭头

--- src/test.sh
+++ src/test.sh
@@ -1,2 +1,2 @@
-gtkc a.c wlbter.c -L. -ltransmission -lutp -ldht -lnatpmp -lminiupnp -levent -lcrypto -lz -pthread -lcurl
-./a.out
+gtkc a.c wlbter.c -L. -ltransmission -lutp -ldht -lnatpmp -lminiupnp -levent -lcrypto -lz -pthread -lcurl \
+&& ./a.out
